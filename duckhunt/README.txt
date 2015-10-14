e2da1ffccf06b3b77cc11d0ee0a0d01d19bff417
Jun Soo Shin <jsshin41@gmail.com>
2015-10-13 22:07:05 -0400

    2nd commit: skeleton for background

---
+++ duckhunt/README.txt
@@ -0,0 +1,16 @@
+README.txt
+by Jun Soo Shin
+Lab 5: README
+due 13 October 2015
+
+What has been correctly implemented and what has not:
+
+
+
+Acknowledgements: 
+
+
+
+Hours spent: 
+
+	
--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,22 @@
-// Replace this with your code...
+/*	
+	game.js
+	by Jun Soo Shin
+	Comp 20: Lab 5
+	due  13 October 2015
+	note: JavaScript code that draws the background and two ducks on the
+	      canvas
+*/
+
+function init() {
+
+	var canvas = document.getElementById("game_canvas");
+	var ctx = canvas.getContext("2d");
+
+	var background = new Image();
+
+	background.addEventListener("load", function() {
+
+	}, false);
+
+	background.src = "duckhunt-background.gif";
+}
