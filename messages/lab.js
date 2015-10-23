7eb951a13fb5254c868966fee297740f672c0c6c
Jun Soo Shin <jsshin41@gmail.com>
2015-10-22 23:10:28 -0400

    create a new variable myRequest and send the request

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,19 @@
-// Your JavaScript goes here...
+/*	
+	game.js
+	by Jun Soo (Derek) Shin
+	Comp 20: Lab 6
+	due  22 October 2015
+	note: JavaScript code that will "load JSON data using XMLHttpRequest 
+	      object, parse the JSON file, and output the data to a section of a
+	      completely loaded HTML file" (Ming).
+*/
+
+function parse() {
+	
+	var myRequest = new XMLHttpRequest();
+	myRequest.open("get", "data.json", true);
+	myRequest.send();
+
+	
+
+}
