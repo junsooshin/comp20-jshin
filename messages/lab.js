253182ecf32e6287ff91ee1f28854e9ef1ef957b
Jun Soo Shin <jsshin41@gmail.com>
2015-10-22 23:37:30 -0400

    trying to see if gh-pages works

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,18 @@
-// Your JavaScript goes here...
+/*	
+	lab.js
+	by Jun Soo Shin
+	Comp 20: Lab 6
+	due  22 October 2015
+	note: JavaScript code that "will load JSON data using JavaScript's 
+	      XMLHttpRequest object, parse the JSON file, and output the data to
+	      a section of a completely loaded HTML file" (Ming).
+*/
+
+function parse() {
+
+	var myRequest = new XMLHttpRequest();
+	myRequest.open("get", "data.json", true);
+	myRequest.send();
+
+
+}
